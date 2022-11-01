af187752eae4f6e8e5d15e2b097f2d46c68ba11b
Eli <elliot.ward@gmail.com>
2022-11-01 10:19:42 +0000

    ECCW-120: ♻️ use dependency injection to remove Node::load() use in event listener.

--- modules/custom/ecc_migrate/src/EventSubscriber/EccMigrateSubscriber.php
+++ modules/custom/ecc_migrate/src/EventSubscriber/EccMigrateSubscriber.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\ecc_migrate\EventSubscriber;
 
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate\Event\MigratePostRowSaveEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -12,14 +14,36 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class EccMigrateSubscriber implements EventSubscriberInterface {
 
   /**
+   * Node storage. For loading nodes in post migrate events.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected EntityStorageInterface $nodeStorage;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Used for obtaining node storage.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->nodeStorage = $entityTypeManager->getStorage('node');
+  }
+
+  /**
    * Migrate post row save event handler.
    *
    * Used to set the author of a node after it has been saved.
-   * This allows us to have separate node and revision IDs, even when there
+   * This allows us to have separate node and revision UIDs, even when there
    * is only one node.
    *
    * @param \Drupal\migrate\Event\MigratePostRowSaveEvent $event
    *   Event being processed.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function onPostRowSave(MigratePostRowSaveEvent $event) {
     // Don't do this for all migrations.
@@ -37,8 +61,7 @@ class EccMigrateSubscriber implements EventSubscriberInterface {
     if ($destination['pseudo_uid'] === $destination['uid']) {
       return;
     }
-    // @todo inject this service.
-    $node = \Drupal\node\Entity\Node::load($event->getDestinationIdValues()[0]);
+    $node = $this->nodeStorage->load($event->getDestinationIdValues()[0]);
     $node->uid = $destination['pseudo_uid'];
     $node->save();
   }
