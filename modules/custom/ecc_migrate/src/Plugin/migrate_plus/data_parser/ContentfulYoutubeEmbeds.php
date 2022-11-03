95cdf19f175d3d7e28c27888f6c773138a31a80c
Eli <elliot.ward@gmail.com>
2022-11-03 10:51:15 +0000

    ECCW-120: ✨ new migrate source plugin for extracting embedded youtube videos so they can be migrated to Drupal media.

--- modules/custom/ecc_migrate/src/Plugin/migrate_plus/data_parser/ContentfulYoutubeEmbeds.php
+++ modules/custom/ecc_migrate/src/Plugin/migrate_plus/data_parser/ContentfulYoutubeEmbeds.php
@@ -3,6 +3,7 @@
 namespace Drupal\ecc_migrate\Plugin\migrate_plus\data_parser;
 
 use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
+use Wa72\HtmlPageDom\HtmlPageCrawler;
 
 /**
  * Extend the Json data parser for embedded YouTube videos.
@@ -30,20 +31,55 @@ use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
  * )
  */
 class ContentfulYoutubeEmbeds extends Json {
+
   /**
    * {@inheritdoc}
    */
   protected function getSourceData(string $url): array {
-    return array_filter(parent::getSourceData($url), function ($value) {
-      if (!isset($value['sys']['contentType']['sys']['id'])) {
-        return FALSE;
-      }
-      if ($value['sys']['contentType']['sys']['id'] != $this->contentType) {
-        return FALSE;
-      }
-      return TRUE;
+    /*
+     * We're getting JSON data as the JsonParser would return it. But we need
+     * to transform that significantly to return a list of the YouTube embeds.
+     */
 
+    // This will get the data for every single content type in the source..
+    $json_source_data = parent::getSourceData($url);
+    // ..so filter out everything that doesn't have a body field containing at
+    // least one YouTube embedded iFrame.
+    $json_source_data = array_filter($json_source_data, function ($value) {
+      if (!isset($value['fields']['body']['en-GB'])) {
+        return FALSE;
+      }
+      if (str_contains(strtolower($value['fields']['body']['en-GB']), 'https://www.youtube.com/embed/')) {
+        return TRUE;
+      }
+      return FALSE;
     });
+
+    // We now need to change the shape of our source data, as rather have each
+    // individual piece of content as a row, we want every individual embed.
+    // First extract all the src elements for YouTube embedded iframes.
+    $youtube_srcs = [];
+    foreach ($json_source_data as $datum) {
+      // We're using the HtmlPageCrawler even though the body field is markdown.
+      // That's because the YouTube embeds are in embedded HTML within the
+      // markdown, and the crawler can still find them.
+      $body_crawler = new HtmlPageCrawler($datum['fields']['body']['en-GB']);
+      $body_crawler->filter('iframe[src]')->each(function (HtmlPageCrawler $node) use (&$youtube_srcs) {
+        $src = $node->getAttribute('src');
+        if (str_starts_with(strtolower($src), 'https://www.youtube.com/embed')) {
+          $youtube_srcs[] = $src;
+        }
+      });
+    }
+    // We don't want to have the same video multiple times.
+    $youtube_srcs = array_unique($youtube_srcs);
+    // Map the sources to an ID so the migration system can address src as a
+    // field.
+    $youtube_source_data = [];
+    foreach ($youtube_srcs as $src) {
+      $youtube_source_data[] = ['id' => $src];
+    }
+    return $youtube_source_data;
   }
 
 }
