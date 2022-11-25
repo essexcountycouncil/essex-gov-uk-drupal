fce5e8933540669ac4f2c753d30c47f2f4875966
Eli <elliot.ward@gmail.com>
2022-11-25 10:04:57 +0000

    ECCW-280: ♻️  change migrate script so that it continues to the next migration if a previous migration fail.

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-drush migrate:import ecc_content_owners && \
-drush migrate:import ecc_users && \
-drush migrate:import ecc_files && \
-drush migrate:import ecc_image_media && \
-drush migrate:import ecc_document_media && \
-drush migrate:import ecc_youtube_embeds && \
-drush migrate:import ecc_newsrooms && \
-drush migrate:import ecc_news && \
-drush migrate:import ecc_guide_pages && \
-drush migrate:import ecc_guide_overviews && \
-drush migrate:import ecc_service_pages && \
-drush migrate:import ecc_service_landing_pages && \
-drush migrate:import ecc_nested_redirects && \
-drush migrate:import ecc_301_redirects && \
+drush migrate:import ecc_content_owners
+drush migrate:import ecc_users
+drush migrate:import ecc_files
+drush migrate:import ecc_image_media
+drush migrate:import ecc_document_media
+drush migrate:import ecc_youtube_embeds
+drush migrate:import ecc_newsrooms
+drush migrate:import ecc_news
+drush migrate:import ecc_guide_pages
+drush migrate:import ecc_guide_overviews
+drush migrate:import ecc_service_pages
+drush migrate:import ecc_service_landing_pages
+drush migrate:import ecc_nested_redirects
+drush migrate:import ecc_301_redirects
 drush migrate:import ecc_302_redirects
