75523bd97adc3b68b7fc294f2953204c159ff436
Eli <elliot.ward@gmail.com>
2022-11-25 11:56:52 +0000

    ECC-122: 🐛 time machine restoration of AlertPages process plugin.

---
+++ web/modules/custom/ecc_migrate/src/Plugin/migrate/process/AlertPages.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\ecc_migrate\Plugin\migrate\process;
+
+use Drupal\Component\Transliteration\TransliterationInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an alert_pages plugin.
+ *
+ * Usage:
+ *
+ * @code
+ * process:
+ *   bar:
+ *     plugin: alert_pages
+ *     source: foo
+ * @endcode
+ *
+ * @MigrateProcessPlugin(
+ *   id = "alert_pages",
+ *   handle_multiples = TRUE
+ * )
+ *
+ * @DCG
+ * ContainerFactoryPluginInterface is optional here. If you have no need for
+ * external services just remove it and all other stuff except transform()
+ * method.
+ */
+class AlertPages extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The transliteration service.
+   *
+   * @var \Drupal\Component\Transliteration\TransliterationInterface
+   */
+  protected $transliteration;
+
+  /**
+   * Constructs an AlertPages plugin.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliteration
+   *   The transliteration service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, TransliterationInterface $transliteration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->transliteration = $transliteration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('transliteration')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!is_array($value)) {
+      throw new \InvalidArgumentException();
+    }
+    $pages = '';
+    foreach ($value as $nid) {
+      $pages .= "node/$nid\n";
+    }
+    return [
+      'request_path' => [
+        'pages' => $pages,
+        'negate' => "0",
+      ],
+    ];
+  }
+
+}
