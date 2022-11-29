32988a3e409784cededd06ef4d88d33f97b0d66f
Eli <elliot.ward@gmail.com>
2022-11-29 09:29:09 +0000

    ECCW-122: 🔥 remove autogenerated code from AlertPages process plugin.

--- web/modules/custom/ecc_migrate/src/Plugin/migrate/process/AlertPages.php
+++ web/modules/custom/ecc_migrate/src/Plugin/migrate/process/AlertPages.php
@@ -2,12 +2,9 @@
 
 namespace Drupal\ecc_migrate\Plugin\migrate\process;
 
-use Drupal\Component\Transliteration\TransliterationInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides an alert_pages plugin.
@@ -25,49 +22,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "alert_pages",
  *   handle_multiples = TRUE
  * )
- *
- * @DCG
- * ContainerFactoryPluginInterface is optional here. If you have no need for
- * external services just remove it and all other stuff except transform()
- * method.
  */
-class AlertPages extends ProcessPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The transliteration service.
-   *
-   * @var \Drupal\Component\Transliteration\TransliterationInterface
-   */
-  protected $transliteration;
-
-  /**
-   * Constructs an AlertPages plugin.
-   *
-   * @param array $configuration
-   *   The plugin configuration.
-   * @param string $plugin_id
-   *   The plugin ID.
-   * @param mixed $plugin_definition
-   *   The plugin definition.
-   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliteration
-   *   The transliteration service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, TransliterationInterface $transliteration) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->transliteration = $transliteration;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('transliteration')
-    );
-  }
+class AlertPages extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
