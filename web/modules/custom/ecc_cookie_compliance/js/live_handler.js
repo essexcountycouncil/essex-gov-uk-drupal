454d3b578c375107392c36528f19f263810ae047
Matthew Wilkes <mwilkes@nomensa.com>
2023-02-24 18:36:08 +0000

    Remove console.logs from debugging

--- web/modules/custom/ecc_cookie_compliance/js/live_handler.js
+++ web/modules/custom/ecc_cookie_compliance/js/live_handler.js
@@ -12,13 +12,11 @@
       } else {
         $.each(document.cookie.split(/; */), function()  {
           var splitCookie = this.split('=');
-          console.log(splitCookie);
           if (splitCookie[0].indexOf('_ga') >= 0) {
             document.cookie = splitCookie[0] + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
           }
         });
       }
-      console.log(categories);
   })
 
 }) (jQuery)
