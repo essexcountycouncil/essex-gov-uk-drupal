80e63a70ada78fc2f1bdf619d0b80723532804ab
Adrian Bateman <abateman@nomensa.com>
2023-11-21 16:59:27 +0000

    ECCW-649: Resolve some of the conflicts with develop.

--- assets/composer/settings.ddev-custom.php
+++ assets/composer/settings.ddev-custom.php
@@ -8,4 +8,3 @@
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/../development.services.yml';
 $config['config_split.config_split.ddev']['status'] = TRUE;
 $config['config_split.config_split.azure']['status'] = FALSE;
-
