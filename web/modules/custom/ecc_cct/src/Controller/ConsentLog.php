b5362f9279f6dcbcfdcf2b3896a896ddecae8d3a
Anthony Lindsay <anthony@annertech.com>
2024-11-21 15:48:02 +0000

    Use dependency injection for accessing db services

--- web/modules/custom/ecc_cct/src/Controller/ConsentLog.php
+++ web/modules/custom/ecc_cct/src/Controller/ConsentLog.php
@@ -5,13 +5,32 @@ declare(strict_types=1);
 namespace Drupal\ecc_cct\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Returns responses for ECC Cookie Consent Tracker routes.
  */
 final class ConsentLog extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('database'));
+  }
+
+  /**
+   * Constructs a database connection service object.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The connection.
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
 
   /**
    * Handles requests to /ecc-cct/js/log/{$choice}.
@@ -26,8 +45,8 @@ final class ConsentLog extends ControllerBase {
    */
   public function log(int $choice, Request $request): Response {
     // If this isn't turned on, bail.
-    if (!\Drupal::config('ecc_cct.settings')->get('enable')) {
-      throw new ServiceUnavailableHttpException(NULL, t('Service unavailable'));
+    if (!$this->config('ecc_cct.settings')->get('enable')) {
+      throw new ServiceUnavailableHttpException(NULL, 'Service unavailable');
     }
     // Otherwise, let's write some data.
     $content = [
@@ -35,9 +54,7 @@ final class ConsentLog extends ControllerBase {
       'message' => "Received ID: $choice",
     ];
 
-    /** @var \Drupal\Core\Database\Connection $connection */
-    $connection = \Drupal::service('database');
-    $result = $connection->insert('ecc_cct_data')
+    $this->connection->insert('ecc_cct_data')
       ->fields([
         'choice' => $choice,
         'timestamp' => time(),
@@ -48,4 +65,5 @@ final class ConsentLog extends ControllerBase {
     // but just in case, for testing, let's leave it in.
     return new Response(json_encode($content), 200, ['Content-Type' => 'application/json']);
   }
+
 }
--- web/modules/custom/ecc_cct/src/Controller/EccCctController.php
+++ web/modules/custom/ecc_cct/src/Controller/EccCctController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\ecc_cct\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for ECC Cookie Consent Tracker routes.
@@ -12,18 +14,33 @@ use Drupal\Core\Controller\ControllerBase;
 final class EccCctController extends ControllerBase {
 
   /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('database'));
+  }
+
+  /**
+   * Constructs a database connection service object.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The connection.
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  /**
    * Builds the response.
    */
   public function build(): array {
     $sql = "SELECT (COUNT(CASE WHEN choice = 1 THEN 1 END) / COUNT(*)) * 100 AS optin FROM {ecc_cct_data};";
-    $database = \Drupal::database();
-    $query = $database->query($sql);
+    $query = $this->connection->query($sql);
     $result = $query->fetchAll();
-
     $stat = $result[0]->optin;
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t($stat . '% of people opted into cookies.'),
+      '#markup' => $this->t('@stat% of people opted into cookies.', ['@stat' => $stat]),
     ];
 
     return $build;
